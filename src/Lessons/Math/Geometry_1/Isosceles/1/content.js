5fe6ff1287fcb6325ee3adb1fcb632712c922d0e
“airladon” <airladon@gmail.com>
2018-12-27 15:41:51 -0800

    split isosceles and equilateral

--- src/Lessons/Math/Geometry_1/Isosceles/1/content.js
+++ src/Lessons/Math/Geometry_1/Isosceles/1/content.js
@@ -44,7 +44,6 @@ class Content extends LessonContent {
     const left = iso._left;
     const right = iso._right;
     const rec = iso._rect;
-    const equil = diag._equil;
 
     let common = {
       setContent: '',
@@ -564,111 +563,6 @@ class Content extends LessonContent {
         'And so we have found that only |one angle| is needed in an |isosceles triangle| to |calculate all three|.',
       ]),
     });
-
-
-    // ***************************************************************
-    // ***************************************************************
-    // ***************************************************************
-    // ***************************************************************
-    // ***************************************************************
-    // ***************************************************************
-    common = {
-      setContent: '',
-      showOnly: [equil, equil._tri],
-      show: [
-        equil._tri._line,
-        equil._tri._side12, equil._tri._side23, equil._tri._side31,
-      ],
-      setSteadyState: () => {
-        equil.setScenario(equil._tri, layout.equil.scenario.center);
-        equil._tri._angle1.label.setText('a');
-        equil._tri._angle1.update();
-        equil._tri._angle2.label.setText('a');
-        equil._tri._angle2.update();
-        equil._tri._angle3.label.setText('a');
-        equil._tri._angle3.update();
-      },
-    };
-    this.addSection(common, {
-      title: 'Equilateral',
-      setContent: [
-        'A triangle that has all |three sides| the |same length| is called an |equilateral| triangle.',
-        `${new Definition('Equilateral', 'Latin', ['aequilateralis', '', 'aequi', 'equal', 'lateralis', 'side']).html('id_lesson__eqiuilateral_definition')}`,
-      ],
-    });
-
-    this.addSection(common, {
-      setContent: 'As |any| two sides are equal, an |equilateral| triangle is a special case of an |isosceles| triangle.',
-      modifiers: {
-        any: click(equil.toggleIsoOrientation, [equil, null, false], colors.equalLength),
-      },
-    });
-
-    this.addSection(common, {
-      setContent: 'Therefore, as |all pairs| of sides are equal, then |all_pairs| of angles are |equal|.',
-      modifiers: {
-        all_pairs: click(equil.toggleIsoOrientation, [equil, null, true], colors.equalLength),
-      },
-    });
-
-    this.addSection(common, {
-      setContent: 'And so, in an |equilateral| triangle, |all_sides| and |all_angles| are |equal|.',
-      modifiers: {
-        all_angles: click(equil.pulseAngles, [equil], colors.angles),
-        all_sides: click(equil.pulseSides, [equil], colors.lines),
-      },
-      show: [
-        equil._tri._line,
-        equil._tri._angle1, equil._tri._angle2, equil._tri._angle3,
-        equil._tri._side12, equil._tri._side23, equil._tri._side31,
-      ],
-    });
-
-    this.addSection({
-      setContent: centerV([
-        'As an equilateral triangle is a special case of an isosceles triangle, then the same method can be used to show |any triangle with equal angles| will also have |equal sides|.',
-        'Therefore, if you |know| a triangle has |equal angles|, you will then know it will be an |equilateral| triangle with |equal sides|.',
-      ]),
-    });
-
-    common.show = [
-      equil._tri._line,
-      equil._tri._angle1, equil._tri._angle2, equil._tri._angle3,
-      equil._tri._side12, equil._tri._side23, equil._tri._side31,
-    ];
-    this.addSection(common, {
-      setContent: [
-        'Next we can consider the |relationship| between an equilateral triangle\'s angles.',
-      ],
-    });
-
-    common.setContent = 'We know all the angles of a triangle sum to |_180|. Therefore, each angle must be a |third of 180º|, which is |60º|.';
-    this.addSection(common, {
-      modifiers: {
-        _180: click(this.showQR, [this, '_triangle_introduction', '_Main'], colors.diagram.action),
-      },
-    });
-
-    this.addSection(common, {
-      modifiers: {
-        _180: click(this.showQR, [this, '_triangle_introduction', '_Main'], colors.diagram.action),
-      },
-      setSteadyState: () => {
-        equil._tri._angle1.label.setText('60º');
-        equil._tri._angle1.update();
-        equil._tri._angle2.label.setText('60º');
-        equil._tri._angle2.update();
-        equil._tri._angle3.label.setText('60º');
-        equil._tri._angle3.update();
-      },
-    });
-
-  // // ***************************************************************
-  // // ***************************************************************
-  // // ***************************************************************
-  // // ***************************************************************
-  // // ***************************************************************
-  // // ***************************************************************
   }
 }
 
--- src/Lessons/Math/Geometry_1/Isosceles/1/diagramCollection.js
+++ src/Lessons/Math/Geometry_1/Isosceles/1/diagramCollection.js
@@ -4,14 +4,12 @@ import Fig from 'figureone';
 import lessonLayout from '../common/layout';
 import CommonLessonDiagram from '../../../../LessonsCommon/CommonLessonDiagram';
 import IsocelesCollection from '../common/diagramCollectionIsoceles';
-import EquilateralCollection from '../common/diagramCollectionEquilateral';
 import CommonDiagramCollection from '../../../../LessonsCommon/DiagramCollection';
 
 const { Transform } = Fig;
 
 export default class DiagramCollection extends CommonDiagramCollection {
   _iso: IsocelesCollection;
-  _equil: EquilateralCollection;
 
   constructor(
     diagram: CommonLessonDiagram,
@@ -21,7 +19,6 @@ export default class DiagramCollection extends CommonDiagramCollection {
     super(diagram, layout, transform);
 
     this.add('iso', new IsocelesCollection(diagram, this.layout));
-    this.add('equil', new EquilateralCollection(diagram, this.layout));
     this.hasTouchableElements = true;
   }
 }
