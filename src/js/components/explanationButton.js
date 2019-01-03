3d062c25aea8c4c159138abc43015a6ef1faa7b6
airladon <airladon@gmail.com>
2019-01-03 09:02:33 -0800

    change rating system to whole stars only, 1-5

--- src/js/components/explanationButton.js
+++ src/js/components/explanationButton.js
@@ -10,24 +10,29 @@ export default class ExplanationButton extends DropDownButton {
     if (listItem.separator === true) {
       return listItem.label;
     }
-    let ratingCss = 'explanation_button__label_rating_value_0';
+    // let ratingCss = 'explanation_button__label_rating_value_0';
     let numReviews = listItem.numReviews || 0;
-    const prefix = 'explanation_button__label_rating_value_';
+    let rating = 0;
+    // const prefix = 'explanation_button__label_rating_value_';
     if (numReviews > 0) {
-      const rating = Math.round((listItem.rating || 0) * 2) / 2;
-      if (rating === 0.5) { ratingCss = `${prefix}0p5`; }
-      if (rating === 1) { ratingCss = `${prefix}1`; }
-      if (rating === 1.5) { ratingCss = `${prefix}1p5`; }
-      if (rating === 2) { ratingCss = `${prefix}2`; }
-      if (rating === 2.5) { ratingCss = `${prefix}2p5`; }
-      if (rating === 3) { ratingCss = `${prefix}3`; }
-      if (rating === 3.5) { ratingCss = `${prefix}3p5`; }
-      if (rating === 4) { ratingCss = `${prefix}4`; }
-      if (rating === 4.5) { ratingCss = `${prefix}4p5`; }
-      if (rating === 5) { ratingCss = `${prefix}5`; }
+      // const rating = Math.round((listItem.rating || 0) * 2) / 2;
+      // if (rating === 0.5) { ratingCss = `${prefix}0p5`; }
+      // if (rating === 1) { ratingCss = `${prefix}1`; }
+      // if (rating === 1.5) { ratingCss = `${prefix}1p5`; }
+      // if (rating === 2) { ratingCss = `${prefix}2`; }
+      // if (rating === 2.5) { ratingCss = `${prefix}2p5`; }
+      // if (rating === 3) { ratingCss = `${prefix}3`; }
+      // if (rating === 3.5) { ratingCss = `${prefix}3p5`; }
+      // if (rating === 4) { ratingCss = `${prefix}4`; }
+      // if (rating === 4.5) { ratingCss = `${prefix}4p5`; }
+      // if (rating === 5) { ratingCss = `${prefix}5`; }
+      rating = '\u2605'.repeat(Math.round(listItem.rating || 0));
+      if (rating === '') {
+        rating = '-';
+      }
       numReviews = (numReviews).toLocaleString('en');
     }
-    const classes = `explanation_button__label_rating_value ${ratingCss}`;
+    // const classes = `explanation_button__label_rating_value ${ratingCss}`;
     let label = '';
     let description = ' ';
     if (listItem.label != null) {
@@ -36,7 +41,7 @@ export default class ExplanationButton extends DropDownButton {
     if (listItem.description != null) {
       ({ description } = listItem);
     }
-    return <div className="explanation_button__label">
+    return <div className="explanation_button__listItem">
       <div className="explanation_button__label_text">
         <div className="explanation_button__label_text_title">
           {label}
@@ -46,9 +51,11 @@ export default class ExplanationButton extends DropDownButton {
         </div>
       </div>
       <div className="explanation_button__label_rating">
-        <div className={classes}/>
+        <div className="explanation_button__label_rating_value">
+          {rating}
+        </div>
         <div className="explanation_button__label_num_reviews">
-        {numReviews}
+          {numReviews}
         </div>
       </div>
     </div>;
