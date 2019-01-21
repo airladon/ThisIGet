71481d99f4f2748e52a17eb5219a3370253a8801
“airladon” <airladon@gmail.com>
2019-01-21 15:26:45 -0800

    dev-server working again

--- containers/dev/start.sh
+++ containers/dev/start.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
-pipenv shell
+if [ $1 ];
+then
+  pipenv run $1
+else
+  pipenv shell
+fi
+
 # /bin/bash
 # if [ $1 ];
 # then
--- start_env.sh
+++ start_env.sh
@@ -35,6 +35,7 @@ if [ $1 = "dev" ];
 then
   HOST_PORT=5002
   CONTAINTER_PORT=5000
+  CMD=/opt/app/start.sh
 fi
 
 if [ $1 = 'dev-server' ];
@@ -42,7 +43,7 @@ if [ $1 = 'dev-server' ];
   HOST_PORT=5003
   CONTAINTER_PORT=5000
   DOCKERFILE="Dockerfile_dev"
-  CMD=/opt/app/dev-server.sh
+  CMD="/opt/app/start.sh /opt/app/dev-server.sh"
 fi
 
 cp containers/$DOCKERFILE Dockerfile
