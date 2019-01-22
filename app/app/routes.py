473897516563355bdc04cf7fd1bd3db04620dd19
airladon <airladon@gmail.com>
2019-01-21 19:48:33 -0800

    more login form

--- app/app/routes.py
+++ app/app/routes.py
@@ -83,5 +83,5 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect('/')
     return render_template('login.html', title='Sign In', form=form)
