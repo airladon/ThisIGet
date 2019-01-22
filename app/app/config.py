852cd423c66a1c0cedb8f3d1296adda77fe3e82a
airladon <airladon@gmail.com>
2019-01-22 08:16:38 -0800

    basic sqlite db with user table

--- app/app/config.py
+++ app/app/config.py
@@ -1,6 +1,10 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or \
         'LHKiusdfuhiDkjhsdf7834897h8y7923'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
