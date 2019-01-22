62d642c2c177fb7925c710a562cc65516368d3ba
“airladon” <airladon@gmail.com>
2019-01-21 17:51:11 -0800

    add flash messages partially

--- app/app/routes.py
+++ app/app/routes.py
@@ -9,7 +9,7 @@
 #     return render_template('index.html')
 
 
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -77,7 +77,11 @@ def chapter1():
     return "Chapter 1 Content"
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
