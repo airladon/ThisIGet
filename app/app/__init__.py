95aee55df369f0a8ce8feb1766318df7d9071617
“airladon” <airladon@gmail.com>
2019-01-21 17:22:06 -0800

    config file working

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
-# from './config' import Config
+from app.config import Config
 
 app = Flask(__name__)
-# app.config.from_object(Config)
-
+app.config.from_object(Config)
+print(app.config)
 from app import routes  # noqa
