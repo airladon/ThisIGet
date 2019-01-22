bdef6dccd234e7b95a0b38421a4d4b781f484139
“airladon” <airladon@gmail.com>
2019-01-22 12:53:09 -0800

    cleanup

--- app/my_app.py
+++ app/my_app.py
@@ -4,6 +4,11 @@ from app.models import db, User, Rating, Category, Comment, Lesson
 # from app.util import assets  # noqa
 
 
+# This is used for debug only
+# type `flask shell` at the venv prompt, and python will start with
+# app, db, User, Rating etc. already imported
+
+
 @app.shell_context_processor
 def make_shell_context():
     return {
