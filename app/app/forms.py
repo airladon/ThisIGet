57dc01db489506010f6e51dee2170a865d776523
airladon <airladon@gmail.com>
2019-01-22 19:19:39 -0800

    cleanup

--- app/app/forms.py
+++ app/app/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Username: ', validators=[DataRequired()])
+    password = PasswordField('Password: ', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
