584a289e3bef2006e5c3bf1da0a4e806bef7415b
airladon <airladon@gmail.com>
2019-01-22 20:04:56 -0800

    basic react form

--- src/js/views/login/login.js
+++ src/js/views/login/login.js
@@ -11,43 +11,54 @@ import ReactDOM from 'react-dom';
 // import { LessonContent } from '../../Lesson/LessonContent';
 // import NavbarSpacer from '../../components/navbarSpacer';
 // import Footer from '../../components/footer';
+class LoginForm extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = { username: '', password: '' };
+
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleUsernameChange(event) {
+    this.setState({ username: event.target.value });
+  }
+
+  handlePasswordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert(`A name was submitted: ${this.state.username} ${this.state.password}`);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.username} onChange={this.handleUsernameChange} />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={this.state.password} onChange={this.handlePasswordChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
 
 const renderlogin = () => {
   const loginId: HTMLElement | null = document.getElementById('login_page');
-  // const lesson = new Lesson(content);
 
   if (loginId instanceof HTMLElement) {
     ReactDOM.render(
       <div>
-      hello world
-      { /*}
-        <form onSubmit={this.handleSubmit}>
-          <FormGroup controlId="email" bsSize="large">
-            <ControlLabel>Email</ControlLabel>
-            <FormControl
-              autoFocus
-              type="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-          </FormGroup>
-          <FormGroup controlId="password" bsSize="large">
-            <ControlLabel>Password</ControlLabel>
-            <FormControl
-              value={this.state.password}
-              onChange={this.handleChange}
-              type="password"
-            />
-          </FormGroup>
-          <Button
-            block
-            bsSize="large"
-            disabled={!this.validateForm()}
-            type="submit"
-          >
-            Login
-          </Button>
-        </form> */}
+      <LoginForm/>
       </div>,
       loginId,
     );
