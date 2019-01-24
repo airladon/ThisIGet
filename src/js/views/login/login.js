99e7697c698f33f4de7f2ed4b872495e26b49c1b
“airladon” <airladon@gmail.com>
2019-01-24 12:16:46 -0800

    formatting almost done

--- src/js/views/login/login.js
+++ src/js/views/login/login.js
@@ -5,82 +5,58 @@ import ReactDOM from 'react-dom';
 import './login.scss';
 
 import Button from '../../components/button';
-import InputFormSubmit from '../../components/inputFormSubmit';
-import InputFormField from '../../components/inputFormField';
-import LoginTitle from '../../components/loginTitle';
-type Props = {};
+// import InputFormSubmit from '../../components/inputFormSubmit';
+// import InputFormField from '../../components/inputFormField';
+// import LoginTitle from '../../components/loginTitle';
+// type Props = {};
 
-type State = {
-  loginFailed: string;
-};
+// type State = {
+//   loginFailed: string;
+// };
 
-class LoginForm extends React.Component<Props, State> {
-  state: State;
-  handleUsernameChange: Function;
-  handlePasswordChange: Function;
-  handleSubmit: Function;
+class LoginForm extends React.Component {
+  // state: State;
+  // handleUsernameChange: Function;
+  // handlePasswordChange: Function;
+  // handleSubmit: Function;
 
-  constructor(props: Props) {
-    super(props);
-    this.state = { loginFailed: '' };
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  // constructor(props: Props) {
+  //   super(props);
+  //   this.state = { loginFailed: '' };
+  //   this.handleSubmit = this.handleSubmit.bind(this);
+  // }
 
 
-  handleSubmit(event) {
-    event.preventDefault();
-    const data = new FormData();
-    const usernameElement = document.getElementById('input_field__username');
-    const passwordElement = document.getElementById('input_field__password');
-    if (usernameElement && passwordElement) {
-      data.append('username', usernameElement.value);
-      data.append('password', passwordElement.value);
-      fetch('/loginuser', {
-        body: data,
-        method: 'post',
-        credentials: 'same-origin',
-        redirect: 'follow',
-      }).then((response) => {
-        if (window.location.href !== response.url) {
-          window.location.replace(response.url);
-        } else {
-          this.setState({ loginFailed: 'Login Failed: Username or Password incorrect.' });
-        }
-      });
-    }
-  }
-
+  // handleSubmit(event) {
+  //   event.preventDefault();
+  //   const data = new FormData();
+  //   const usernameElement = document.getElementById('input_field__username');
+  //   const passwordElement = document.getElementById('input_field__password');
+  //   if (usernameElement && passwordElement) {
+  //     data.append('username', usernameElement.value);
+  //     data.append('password', passwordElement.value);
+  //     fetch('/loginuser', {
+  //       body: data,
+  //       method: 'post',
+  //       credentials: 'same-origin',
+  //       redirect: 'follow',
+  //     }).then((response) => {
+  //       if (window.location.href !== response.url) {
+  //         window.location.replace(response.url);
+  //       } else {
+  //         this.setState({ loginFailed: 'Login Failed: Username or Password incorrect.' });
+  //       }
+  //     });
+  //   }
+  // }
+  // eslint-disable-next-line class-methods-use-this
   render() {
     return (
       <div>
-        <div className="login_form">
-          <div className="login_centering_cell">
-          <LoginTitle title="Sign in to ItIGet"/>
-            <form onSubmit={this.handleSubmit} id="login_form">
-              <InputFormField
-                label="Username or Email:"
-                onError=""
-                autoComplete="username"
-                type="text"
-                id="input_field__username"
-              />
-              <InputFormField
-                label="Password:"
-                onError=""
-                autoComplete="current-password"
-                type="password"
-                id="input_field__password"
-              />
-              <InputFormSubmit
-                label="Sign in"
-                onError={this.state.loginFailed}
-              />
-            </form>
-          </div>
-        </div>
-        <div className="login_centering_cell">
-          <Button href="/create" className="login_button login_button_create">Create Accout</Button>
-          <Button href="/reset" className="login_button login_button_forgot">Forgot Password</Button>
+        <div className="input_form__footer"/>
+        <div className="input_form__centering_cell">
+          <Button href="/create" className="input_form__link input_form__link_create">Create Accout</Button>
+          <Button href="/reset" className="input_form__link input_form__link_forgot">Forgot Password</Button>
         </div>
       </div>
     );
