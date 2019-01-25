9125b0acebaaa8e3a886265470b642085d864979
“airladon” <airladon@gmail.com>
2019-01-24 17:54:26 -0800

    ongoing mail setup

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -3,11 +3,13 @@ from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+mail = Mail(app)
 
 from app import routes, models  # noqa
