008edac1e4d256dc826f700168d04ecc71e44443
airladon <airladon@gmail.com>
2019-02-05 06:25:01 -0800

    update icons

--- src/js/views/home/banner.js
+++ src/js/views/home/banner.js
@@ -10,7 +10,7 @@ export default class HomeBanner extends React.Component
       <div className="home__banner_logo">
         <div className="home__banner_logo_text_container">
           <div className="home__banner_logo_text">
-            get it
+            this i get
           </div>
         </div>
       </div>
