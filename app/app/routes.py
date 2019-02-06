afaee005f230a4e440ad81f7ca164233a947ce65
“airladon” <airladon@gmail.com>
2019-02-06 14:02:11 -0800

    Add check_confirmed decorator

--- app/app/routes.py
+++ app/app/routes.py
@@ -22,6 +22,19 @@ import datetime
 from app.tools import hash_str_with_pepper
 # import pdb
 
+# project/decorators.py
+from functools import wraps
+
+def check_confirmed(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        if current_user.confirmed is False:
+            flash('Please confirm your account!', 'warning')
+            return redirect(f'confirmAccountEmailSent/{user.username}')
+        return func(*args, **kwargs)
+
+    return decorated_function
+
 
 @app.route('/')
 def home():
