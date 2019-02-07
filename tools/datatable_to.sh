48f544976d51cff34b558af07291435e52ff92f8
airladon <airladon@gmail.com>
2019-02-06 18:49:56 -0800

    update tool

--- tools/datatable_to.sh
+++ tools/datatable_to.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env sh
 
-URL=''
+URL=URL=postgresql://postgres@host.docker.internal
 DB_NAME=thisiget_local
-case $1 in
-  local ) URL=postgresql://postgres@localhost;;
-  docker ) URL=postgresql://postgres@host.docker.internal;;
-  * ) URL=postgresql://postgres@host.docker.internal;;
-esac
+if [ $1 ];
+then
+  case $1 in
+    local ) URL=postgresql://postgres@localhost;;
+    docker ) URL=postgresql://postgres@host.docker.internal;;
+    * ) URL=`heroku config:get DATABASE_URL --app=$1`;;
+  esac
+fi
 echo
 echo export DATABASE_URL=$URL/$DB_NAME
 echo export DATABASE_URL=$URL/$DB_NAME | pbcopy
