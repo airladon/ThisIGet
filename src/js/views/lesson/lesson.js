df064e8ed86d3852208d7b53c97194614b34d187
“airladon” <airladon@gmail.com>
2019-02-07 17:42:40 -0800

    working on moving login management up to highest page

--- src/js/views/lesson/lesson.js
+++ src/js/views/lesson/lesson.js
@@ -9,6 +9,34 @@ import Lesson from '../../Lesson/Lesson';
 import { LessonContent } from '../../Lesson/LessonContent';
 import NavbarSpacer from '../../components/navbarSpacer';
 import Footer from '../../components/footer';
+import { fetch as fetchPolyfill } from 'whatwg-fetch';    // Fetch polyfill
+
+function checkIsLoggedInCookie() {
+  const { cookie } = document;
+  if (cookie != null) {
+    // $FlowFixMe
+    const username = cookie.match(/username=[^;]*;/);
+    if (username != null) {
+      this.setLogin(username[0]
+        .split('=')[1]
+        .slice(0, -1));
+    }
+  }
+}
+
+function checkIsLoggedInAPI() {
+  // console.log('checking1')
+  fetchPolyfill('/isloggedin', { credentials: 'same-origin' })
+    .then((response) => {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      // console.log(response, response.json());
+      return response.json();
+    })
+    .then(data => this.setLogin(data.username))
+    .catch(() => {});
+}
 
 const renderLesson = (content: LessonContent, lessonDetails: Object, versionDetails: Object) => {
   const lessonId: HTMLElement | null = document.getElementById('single-page-lesson');
