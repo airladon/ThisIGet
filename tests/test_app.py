47c0e1df62118fc7c9f68cd1d0ee92aaebea53ac
“airladon” <airladon@gmail.com>
2019-02-10 17:29:27 -0800

    working on second test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
 import pytest  # noqa: F401
-# import os
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 # import tempfile
 import pdb
 import sys
@@ -7,19 +8,29 @@ sys.path.insert(0, './app/')
 from app import app
 from app.models import db, Users  # noqa
 
-# @pytest.mark.filterwarnings("DeprecationWarning")
-def test1():
+
+@pytest.fixture
+def client():
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app_test.db')
     client = app.test_client()
+    yield client
 
+
+def test_root_page_cookie(client):
     result = client.get('/')
-    # pdb.set_trace()
-
-    # print(result)
     assert result.headers['Set-Cookie'] == 'username=; Path=/'
 
-    assert result.data == b'''<!doctype html>\n\n<html lang="en">\n  <head>\n    <!-- Required meta tags -->\n    <meta charset="utf-8">\n    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">\n    \n    \n  <link rel="stylesheet" type="text/css" href="/static/dist/main.css">\n\n\n    <link rel="stylesheet" href="/static/vendors/bootstrap-reboot.min.css">\n\n    \n    <script src="/static/index.js"></script>\n  </head>\n\n<body>\n  <!--  -->\n  \n    <div id="home"></div>\n\n<script type="text/javascript" src="/static/dist/vendors.js"></script>\n<script type="text/javascript" src="/static/dist/tools.js"></script>\n<script type="text/javascript" src="/static/dist/commonlessons.js"></script>\n\n  <script type="text/javascript" src="/static/dist/main.js"></script>\n\n</body>\n</html>'''
+
+def login(client, username, password):
+    return client.post('/login', data=dict(
+        username=username,
+        password=password
+    ), follow_redirects=True)
 
 
+def test_login(client):
+    result = login(client, 'airladon', 'asdfasdf')
+    pdb.set_trace()
 # @pytest.fixture
 # def client():
 #     db, app.app.config['DATABASE'] = tempfile.mkstemp()
