434c2bb26cc1bb05280ad14b7e3a8a1cca5a1a10
“airladon” <airladon@gmail.com>
2019-02-10 16:52:05 -0800

    start flask testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,25 @@
 import pytest  # noqa: F401
+import os
+import tempfile
+import sys
+sys.path.insert(0, './app/')
+from app import app
+from app.models import db, Users  # noqa
 
+def test1():
+    app.
 
-def test_default():
-    assert True
+# @pytest.fixture
+# def client():
+#     db, app.app.config['DATABASE'] = tempfile.mkstemp()
+#     # app.app.config['TESTING'] = True
+#     client = app.app.test_client()
+
+#     with flaskr.app.app_context():
+#         flaskr.init_db()
+
+#     yield client
+
+#     os.close(db_fd)
+#     os.unlink(flaskr.app.config['DATABASE'])
+
