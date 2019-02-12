d0c7256add628cbba0fd4bcbbfa6c8c0989a218e
airladon <airladon@gmail.com>
2019-02-11 19:13:28 -0800

    jest-puppeteer working in container

--- tests/browser/test.test.js
+++ tests/browser/test.test.js
@@ -1,21 +1,21 @@
 
-// describe('Google', () => {
-//   beforeAll(async () => {
-//     await page.goto('https://google.com');
-//   });
+describe('Google', () => {
+  beforeAll(async () => {
+    await page.goto('https://google.com');
+  });
 
-//   it('should be titled "Google"', async () => {
-//     await expect(page.title()).resolves.toMatch('Google');
-//   });
-// });
+  it('should be titled "Google"', async () => {
+    await expect(page.title()).resolves.toMatch('Google');
+  });
+});
 
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-  const page = await browser.newPage();
-  await page.goto('https://www.google.com');
-  await page.screenshot({ path: 'google1.png' });
+// (async () => {
+//   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+//   const page = await browser.newPage();
+//   await page.goto('https://www.google.com');
+//   await page.screenshot({ path: 'google1.png' });
 
-  await browser.close();
-})();
+//   await browser.close();
+// })();
