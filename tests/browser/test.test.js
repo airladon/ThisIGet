dd28b99a4d186f17e73c45026236db5e474b61fb
airladon <airladon@gmail.com>
2019-02-12 06:49:03 -0800

    add tests for Angles lesson

--- tests/browser/test.test.js
+++ tests/browser/test.test.js
@@ -10,6 +10,7 @@ const testMode = process.env.TEST_MODE || 'test';
 describe('Home Page', () => {
   beforeAll(async () => {
     await page.goto(sitePath);
+    await page.setViewport({ width: 500, height: 1500 });
   });
   it('Should be titled "This I Get"', async () => {
     // if (testMode === 'prod') { return; }
@@ -19,6 +20,38 @@ describe('Home Page', () => {
   });
   it('Should have no visutal regression', async () => {
     const image = await page.screenshot({ path: 'main.png' });
+    expect(image).toMatchImageSnapshot();
+  });
+});
+
+
+const anglesPath = `${sitePath}/Lessons/Math/Geometry_1/Angle/base/explanation`;
+
+describe('Angles Lesson', () => {
+  beforeAll(async () => {
+    await page.goto(anglesPath);
+    await page.setViewport({ width: 600, height: 800 });
+  });
+  // it('Should be titled "This I Get"', async () => {
+  //   // if (testMode === 'prod') { return; }
+  //   // await console.log(page.title())
+  //   // const text = await page.evaluate(() => document.body.textContent);
+  //   await expect(page.title()).resolves.toMatch('This I Get');
+  // });
+  it('Page 1 should have no visutal regression', async () => {
+    // await jestPuppeteer.debug();
+    // const text = await page.evaluate(() => document.body.textContent);
+    // console.log(anglesPath)
+    // console.log(text)
+    await page.goto(anglesPath);
+    await page.setViewport({ width: 600, height: 800 });
+    const image = await page.screenshot({ path: 'anglesPage1.png' });
+    expect(image).toMatchImageSnapshot();
+  });
+  it('Page 6 should have no visutal regression', async () => {
+    await page.goto(`${anglesPath}?page=6`);
+    await page.setViewport({ width: 600, height: 800 });
+    const image = await page.screenshot({ path: 'anglesPage6.png' });
     expect(image).toMatchImageSnapshot();
   });
 });
