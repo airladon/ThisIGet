afc1e4d54b79e3b5ec1da8be2c04d63ff98ff5d7
“airladon” <airladon@gmail.com>
2019-02-28 15:57:59 -0800

    add wheel and circle

--- src/Lessons/Math/Geometry_1/Introduction/singlePage/explanation/content.js
+++ src/Lessons/Math/Geometry_1/Introduction/singlePage/explanation/content.js
@@ -50,14 +50,19 @@ class Content extends SinglePageLessonContent {
       vertexShader: 'withTexture',
       fragmentShader: 'withTexture',
     }, layout);
-    this.diagram.elements.add(
-      'circles',
-      new Collection(
-        this.diagram,
-        layout,
-        new Transform('circles').scale(1, 1).translate(0, 0),
-      ),
-    );
+    // this.diagram.elements.add(
+    //   'circles',
+    //   new Collection(
+    //     this.diagram,
+    //     layout,
+    //     new Transform('circles').scale(1, 1).translate(0, 0),
+    //   ),
+    // );
+    this.diagram.setElementsToCollection(new Collection(
+      this.diagram,
+      layout,
+      new Transform('circles').scale(1, 1).translate(0, 0),
+    ));
   }
 
   // Array of strings, html or jsx
@@ -66,24 +71,25 @@ class Content extends SinglePageLessonContent {
     const diag = this.diagram.elements;
     console.log(diag)
 
+    // Set figure one initial conditions
+    diag._fig1._wheel.setScenario('left');
+    diag._fig1._circle.setScenario('right');
+
     const content = [
       '# Shapes',
       '|Mathematics is a powerful tool| that we use to |understand| and |predict| the world around us.',
-      'Mathematics describes an object or phenomenon in a more |simple|, and more |general| way. Describing something more |simply|, makes it easier to study and understand. Describing something more |generally|, means the understanding can be reapplied to other scenarios.',
-      'The process of describing something in a more general way is |abstraction|.',
+      'Mathematics describes an object or phenomenon in a more |simple|, and more |general| way. Describing something more |simply|, makes it easier to study and understand. Describing something more |generally|, means the understanding can be reapplied to other scenarios. The process of describing something in a more general way is |abstraction|.',
       'A large area of mathematics is the study of |shapes|. Shapes are simple abstractions of |objects| and the |paths| they travel.',
-      'For example, a |wheel| is a physical thing. It is made of different materials, has mass, size, location and smell. However, these details are ',
-      makeFig('id_figure1', diag._circles._wheel),
+      'For example, a |wheel| is a physical thing. It is made of different materials, has mass, size, location and smell. A wheel can be abstracted into a |_shape_| by removing a lot of these details, but keeping the outline.',
+      makeFig('id_figure1', diag._fig1),
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam varius ipsum eget euismod vestibulum. Sed sit amet sollicitudin lacus. Fusce varius nisl non elementum dictum. Nulla tellus leo, aliquam eget facilisis vitae, pulvinar at magna. Praesent dignissim feugiat cursus. Maecenas convallis ac dolor nec luctus. Vestibulum sollicitudin ante eu nisl finibus, ut posuere tortor sagittis. Donec quam lectus, tristique at quam in, semper volutpat sapien. Mauris eu est mollis magna bibendum volutpat. Integer lacinia convallis euismod. Duis consectetur libero purus, vel molestie dui condimentum semper. In pretium enim accumsan neque egestas, non ultricies massa ultrices.',
       'Shapes are |amazing|',
       'Shapes are |amazing|',
-      <div key={this.key += 1} id="wss_collection_1"></div>,
       'Shapes are |amazing|',
       '## Shapes here',
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam varius ipsum eget euismod vestibulum. Sed sit amet sollicitudin lacus. Fusce varius nisl non elementum dictum. Nulla tellus leo, aliquam eget facilisis vitae, pulvinar at magna. Praesent dignissim feugiat cursus. Maecenas convallis ac dolor nec luctus. Vestibulum sollicitudin ante eu nisl finibus, ut posuere tortor sagittis. Donec quam lectus, tristique at quam in, semper volutpat sapien. Mauris eu est mollis magna bibendum volutpat. Integer lacinia convallis euismod. Duis consectetur libero purus, vel molestie dui condimentum semper. In pretium enim accumsan neque egestas, non ultricies massa ultrices.',
       'Shapes are |amazing|',
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam varius ipsum eget euismod vestibulum. Sed sit amet sollicitudin lacus. Fusce varius nisl non elementum dictum. Nulla tellus leo, aliquam eget facilisis vitae, pulvinar at magna. Praesent dignissim feugiat cursus. Maecenas convallis ac dolor nec luctus. Vestibulum sollicitudin ante eu nisl finibus, ut posuere tortor sagittis. Donec quam lectus, tristique at quam in, semper volutpat sapien. Mauris eu est mollis magna bibendum volutpat. Integer lacinia convallis euismod. Duis consectetur libero purus, vel molestie dui condimentum semper. In pretium enim accumsan neque egestas, non ultricies massa ultrices.',
-      <div key={this.key += 1} id="wss_collection_2"></div>,
       'Shapes are |amazing|',
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam varius ipsum eget euismod vestibulum. Sed sit amet sollicitudin lacus. Fusce varius nisl non elementum dictum. Nulla tellus leo, aliquam eget facilisis vitae, pulvinar at magna. Praesent dignissim feugiat cursus. Maecenas convallis ac dolor nec luctus. Vestibulum sollicitudin ante eu nisl finibus, ut posuere tortor sagittis. Donec quam lectus, tristique at quam in, semper volutpat sapien. Mauris eu est mollis magna bibendum volutpat. Integer lacinia convallis euismod. Duis consectetur libero purus, vel molestie dui condimentum semper. In pretium enim accumsan neque egestas, non ultricies massa ultrices.',
       'Shapes are |amazing|',
