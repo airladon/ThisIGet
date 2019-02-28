edcc5e11bc25a144ecd12bce078520e361f6f1d3
“airladon” <airladon@gmail.com>
2019-02-27 17:53:44 -0800

    updates

--- src/Lessons/Math/Geometry_1/Introduction/simple/explanation/lesson.js
+++ src/Lessons/Math/Geometry_1/Introduction/simple/explanation/lesson.js
@@ -3,7 +3,7 @@
 import renderLesson from '../../../../../../js/views/lesson/lesson';
 import SimpleLesson from '../../../../../../js/Lesson/SimpleLesson';
 import Content from './content';
-// import '../common/style.scss';
+import './style.scss';
 // import '../../../geometry.scss';
 import details from '../../details';
 import version from '../version';
