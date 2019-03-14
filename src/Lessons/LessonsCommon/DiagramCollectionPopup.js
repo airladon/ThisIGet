ecb019b647ca146e40582ffcf7f17e3b35c720f4
“airladon” <airladon@gmail.com>
2019-03-14 14:59:24 -0700

    cleanup

--- src/Lessons/LessonsCommon/DiagramCollectionPopup.js
+++ src/Lessons/LessonsCommon/DiagramCollectionPopup.js
@@ -225,19 +225,6 @@ export default class PopupBoxCollection extends CommonDiagramCollection {
 
   show() {
     super.show();
-    // const interactiveButton = document.getElementById('id_lesson__interactive_element_button__container');
-    // if (interactiveButton instanceof HTMLElement) {
-    //   interactiveButton.classList.add('lesson__interactive_element_button__disable');
-    // }
-    // const actionElements = document.getElementsByClassName('action_word_enabled');
-    // if (actionElements) {
-    //   for (let i = 0; i < actionElements.length; i += 1) {
-    //     const element = actionElements[i];
-    //     if (!element.classList.contains('lesson__popup_box__action_word')) {
-    //       element.classList.add('lesson__action_word_disabled_by_popup');
-    //     }
-    //   }
-    // }
     this._box.show();
   }
 
@@ -246,23 +233,8 @@ export default class PopupBoxCollection extends CommonDiagramCollection {
   }
 
   hideAll() {
-    // this.prepareToHideQR();
     this.prepareToHideAll();
     super.hideAll();
-    // this.diagram.container.style.zIndex = '-1';
-    // const interactiveButton = document.getElementById('id_lesson__interactive_element_button__container');
-    // if (interactiveButton instanceof HTMLElement) {
-    //   interactiveButton.classList.remove('lesson__interactive_element_button__disable');
-    // }
-    // const actionElements = document.getElementsByClassName('action_word_enabled');
-    // if (actionElements) {
-    //   for (let i = 0; i < actionElements.length; i += 1) {
-    //     const element = actionElements[i];
-    //     if (!element.classList.contains('lesson__popup_box__action_word')) {
-    //       element.classList.remove('lesson__action_word_disabled_by_popup');
-    //     }
-    //   }
-    // }
     this.diagram.animateNextFrame();
   }
 }
