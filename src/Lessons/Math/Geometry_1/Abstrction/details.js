6d6ce70a0e48a597440e36164a2f79dcb699e3c5
airladon <airladon@gmail.com>
2019-03-20 08:02:56 -0700

    add abstraction

---
+++ src/Lessons/Math/Geometry_1/Abstrction/details.js
@@ -0,0 +1,12 @@
+// @flow
+
+// eslint-disable-next-line no-var
+var details = {
+  title: 'Circles',
+  uid: 'circles',
+  dependencies: ['why_study_shapes'],
+};
+
+module.exports = {
+  details,
+};
