afece0380ec5791d826a0715f9d06a4433d826d2
airladon <airladon@gmail.com>
2019-03-20 06:43:27 -0700

    test cleanup

--- src/Lessons/Math/Geometry_1/Introduction/singlePage/tests/introduction-single-page.btest.js
+++ src/Lessons/Math/Geometry_1/Introduction/singlePage/tests/introduction-single-page.btest.js
@@ -11,7 +11,32 @@ expect.extend({ toMatchImageSnapshot });
 //   return new Promise(resolve => setTimeout(resolve, ms));
 // }
 
+// const pageTest = async (path, fileName, delay = 0, scrollTo = 0) => {
+//   const url = `${sitePath}/${path}`;
+//   await page.goto(url);
+//   await page.setViewport({ width: 600, height: 800 });
+//   await page.evaluate(() => {
+//     window.scrollTo(0, scrollTo);
+//   });
+//   sleep(delay);
+//   const image = await page.screenshot({ path: `${fileName}.png` });
+//   expect(image).toMatchImageSnapshot();
+// };
+
 describe(`${details.details.uid} - ${version.details.uid}`, () => {
+  // let pageTest;
+  // beforeEach(() => {
+  //   pageTest = async (path, fileName, delay = 0, scrollTo = 0) => {
+  //     const url = `${sitePath}/${path}`;
+  //     await page.goto(url);
+  //     await page.setViewport({ width: 600, height: 800 });
+  //     await page.evaluate(() => {
+  //       window.scrollTo(0, scrollTo);
+  //     });
+  //     const image = await page.screenshot({ path: `${fileName}.png` });
+  //     expect(image).toMatchImageSnapshot();
+  //   };
+  // });
   test('Top of page', async () => {
     const anglesPath =
       `${sitePath}/Lessons/Math/Geometry_1/Introduction/singlePage/explanation`;
@@ -22,6 +47,10 @@ describe(`${details.details.uid} - ${version.details.uid}`, () => {
     });
     const image = await page.screenshot({ path: 'top.png' });
     expect(image).toMatchImageSnapshot();
+    // pageTest(
+    //   '/Lessons/Math/Geometry_1/Introduction/singlePage/explanation',
+    //   'top',
+    // );
   });
 
   test('Middle of page', async () => {
@@ -34,5 +63,8 @@ describe(`${details.details.uid} - ${version.details.uid}`, () => {
     });
     const image = await page.screenshot({ path: 'middle.png' });
     expect(image).toMatchImageSnapshot();
+    // pageTest(
+    //   '/Lessons/Math/Geometry_1/Introduction/singlePage/explanation',
+    //   'middle', 0, 500);
   });
 });
