4052e4b98f95f1e22af00cdad6aae34dc1fcb414
airladon <airladon@gmail.com>
2019-04-13 07:41:55 -0700

    aaa movable top point

--- src/Lessons/Math/Geometry_1/CongruentTriangles/base/common/aaa.js
+++ src/Lessons/Math/Geometry_1/CongruentTriangles/base/common/aaa.js
@@ -27,6 +27,9 @@ export default class CommonCollectionAAA extends CommonDiagramCollection {
     } & DiagramObjectPolyLine;
   } & DiagramElementCollection;
 
+  leftAngle: number;
+  rightAngle: number;
+
   constructor(
     diagram: CommonLessonDiagram,
     layout: Object,
@@ -39,26 +42,50 @@ export default class CommonCollectionAAA extends CommonDiagramCollection {
   }
 
   resetTri() {
+    this.leftAngle = Math.PI / 6;
+    this.rightAngle = Math.PI / 3 * 2;
     const tri = this._fig._tri;
     tri._pad2.move.limitLine = new Line(new Point(-2.2, -1), new Point(-1, -1));
     tri._pad1.move.limitLine = new Line(new Point(2.2, -1), new Point(1, -1));
     tri._pad1.makeTouchable();
     tri._pad2.setTransformCallback = this.updateTri.bind(this);
     tri._pad1.setTransformCallback = this.updateTri.bind(this);
-    tri._pad0.setColor(this.colors.diagram.background);
-    tri._pad0.isTouchable = false;
-    tri._pad0.isInteractive = false;
-    tri._pad0.isMovable = false;
+    tri._pad0.setTransformCallback = this.updateTop.bind(this);
+    // tri._pad0.setMoveBoundaryToDiagram([-1, -1, 2, 2])
+    
+    // tri._pad0.setColor(this.colors.diagram.background);
+    // tri._pad0.isTouchable = false;
+    // tri._pad0.isInteractive = false;
+    // tri._pad0.isMovable = false;
+  }
+
+  updateTop() {
+    const tri = this._fig._tri;
+    const top = tri._pad0.getPosition();
+    const left = tri._pad2.getPosition();
+    const right = tri._pad1.getPosition();
+    this.leftAngle = new Line(left, top).angle();
+    this.rightAngle = new Line(right, top).angle();
+    this.updateTri();
   }
 
   updateTri() {
     const tri = this._fig._tri;
     const left = tri._pad2.getPosition();
     const right = tri._pad1.getPosition();
-    const leftTop = new Line(left, 5, Math.PI / 6);
-    const rightTop = new Line(right, 5, Math.PI / 3 * 2);
+    const leftTop = new Line(left, 5, this.leftAngle);
+    const rightTop = new Line(right, 5, this.rightAngle);
     const top = leftTop.intersectsWith(rightTop).intersect;
     // console.log(top)
     tri.updatePoints([top, right, left]);
+
+    tri._pad0.move.maxTransform.updateTranslation(
+      Math.max(1, top.x),
+      Math.max(1.5, top.y),
+    );
+    tri._pad0.move.minTransform.updateTranslation(
+      Math.min(-1, top.x),
+      Math.min(0, top.y),
+    );
   }
 }
