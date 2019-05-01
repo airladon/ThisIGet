cf7a4b4e7547da6c57f93873ff657d628712abd9
airladon <airladon@gmail.com>
2019-04-30 20:01:27 -0700

    initial external angles

---
+++ src/Lessons/Math/Geometry_1/ExternalAngles/base/common/layout.js
@@ -0,0 +1,24 @@
+// @flow
+import Fig from 'figureone';
+import baseLayout from '../../../../../LessonsCommon/layout';
+
+const {
+  Point,
+  // Transform,
+  // Line,
+} = Fig.tools.g2;
+
+// const { joinObjects } = Fig.tools.misc;
+
+const cssColorNames = [
+  // 'lines',
+];
+
+/* eslint-disable key-spacing, comma-spacing, no-multi-spaces, space-in-parens */
+export default function lessonLayout() {
+  const layout: Object = baseLayout();
+  layout.colors = Fig.tools.color.getCSSColors(cssColorNames);
+  const { colors } = layout;
+  layout.addElements = [];
+  return layout;
+}
