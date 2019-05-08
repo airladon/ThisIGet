03ec3008c8c9bd9e78074ed3b8072b41d06091bf
“airladon” <airladon@gmail.com>
2019-05-07 17:44:19 -0700

    fix areacircle

--- src/Lessons/Math/Geometry_1/AreaCircle/base/tests/tests.btest.js
+++ src/Lessons/Math/Geometry_1/AreaCircle/base/tests/tests.btest.js
@@ -3,6 +3,11 @@ import 'babel-polyfill';
 import tester from '../../../../../testers/presentationLessonTester';
 
 tester(
+  {
+    pages: {
+      27: { threshold: 0.000025 },
+    },
+  },
   'explanation',
   'summary',
   // [
