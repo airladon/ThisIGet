836688afc0035f7e34c8cc7c6583040982573351
“airladon” <airladon@gmail.com>
2019-05-08 15:34:31 -0700

    add rating tester

--- src/Lessons/testers/presentationLessonTester.js
+++ src/Lessons/testers/presentationLessonTester.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import 'babel-polyfill';
 import { toMatchImageSnapshot } from 'jest-image-snapshot';
+import joinObjects from './tools';
 
 const fs = require('fs');
 
@@ -12,51 +13,6 @@ function contentSectionCount(testPath, topicName) {
   fileName = `${fileName}/${topicName}/content.js`;
   const content = fs.readFileSync(fileName, 'utf8');
   return (content.match(/\n *this\.addSection/g) || []).length;
-}
-
-function joinObjects(...objects) {
-  const assignObjectFromTo = (fromObject, toObject) => {
-    Object.keys(fromObject).forEach((key) => {
-      const value = fromObject[key];
-      if (typeof value === 'number'
-        || typeof value === 'boolean'
-        || typeof value === 'string'
-        || value == null
-        || typeof value === 'function'
-        || typeof value._dup === 'function'
-        || Array.isArray(value)
-      ) {
-        // console.log(value, toObject[key])
-        if (value !== undefined || toObject[key] === undefined) {
-          // eslint-disable-next-line no-param-reassign
-          toObject[key] = value;
-        }
-      } else {
-        const toValue = toObject[key];
-        if (typeof toValue === 'number'
-          || typeof toValue === 'boolean'
-          || typeof toValue === 'string'
-          || toValue == null
-          || typeof toValue === 'function'
-          || Array.isArray(toValue)
-        ) {
-          // eslint-disable-next-line no-param-reassign
-          toObject[key] = {};
-        }
-        assignObjectFromTo(value, toObject[key]);
-      }
-    });
-  };
-
-  const num = objects.length;
-  const out = objects[0];
-  for (let i = 1; i < num; i += 1) {
-    const o = objects[i];
-    if (o != null) {
-      assignObjectFromTo(o, out);
-    }
-  }
-  return out;
 }
 
 // tester(
