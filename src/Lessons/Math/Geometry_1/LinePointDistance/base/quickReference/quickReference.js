654b6a1aec278d87bbed6d55322df72aee51bae7
“airladon” <airladon@gmail.com>
2019-05-09 15:13:07 -0700

    qr done

--- src/Lessons/Math/Geometry_1/LinePointDistance/base/quickReference/quickReference.js
+++ src/Lessons/Math/Geometry_1/LinePointDistance/base/quickReference/quickReference.js
@@ -5,16 +5,16 @@ import lessonLayout from './layout';
 import PopupBoxCollection from '../../../../../LessonsCommon/DiagramCollectionPopup';
 import details from '../../details';
 import version from '../version';
-import CommonCollection from '../common/diagramCollectionCommon';
+import CommonCollection from './collection';
 
 const { Transform, Rect } = Fig;
-// const {
-//   click,
+const {
+  click,
 //   highlight,
 //   clickWord,
-// } = Fig.tools.html;
+} = Fig.tools.html;
 
-export default class QRBoilerplate extends PopupBoxCollection {
+export default class QRPointLineDistance extends PopupBoxCollection {
   _collection: CommonCollection;
 
   constructor(
@@ -31,21 +31,33 @@ export default class QRBoilerplate extends PopupBoxCollection {
     );
     this.hasTouchableElements = true;
 
-    const modifiers = {};
-    this.setTitle('');
-    this.setDescription(`
-      <p>
-      </p>
-    `, modifiers);
+    const coll = this._collection;
+    const { colors } = this.layout;
+    const modifiers = {
+      point: click(coll.pulsePoints, [coll], colors.points),
+      point_: click(coll.pulsePointEnd, [coll], colors.points),
+      point__: click(coll.pulsePoint, [coll], colors.points),
+      point___: click(coll.pulsePoint, [coll], colors.points),
+      line: click(coll.pulseLine, [coll], colors.lines),
+      line_: click(coll.pulseLine, [coll], colors.lines),
+      perpendicular_line: click(coll.pulsePerpendicularLabel, [coll, null], colors.distance),
+      point_and_line_end: click(coll.pulseDistanceEnd, [coll], colors.distance2),
+      line_end: click(coll.pulseEnd, [coll], colors.lines),
+    };
+    this.setTitle('Point Line Distance');
+    this.setDescription('The distance between a |point| and a |line| is the |shortest distance|. When the |point_| is closest to a |line_end|, then the distance is between the |point_and_line_end|. When the |point__| is |not closest| to a line end, then the shortest distance is the |perpendicular_line| between |point___| and |line_|. ', modifiers);
     this.setLink(details.details.uid);
   }
 
   show() {
-    this.setDiagramSpace({ location: 'top', ySize: 0.7, xSize: 0.5 });
+    this.setDiagramSpace({ location: 'top', ySize: 0.5, xSize: 0.5 });
     super.show();
     const collection = this._collection;
-    collection.show();
-    this.transformToQRWindow(collection, new Rect(-2, -1.4, 4, 2.4));
+    const coll = this._collection;
+    const fig = coll._fig;
+    fig.setScenario('low');
+    collection.showAll();
+    this.transformToQRWindow(collection, new Rect(-2, -1.3, 4, 2));
     this.diagram.animateNextFrame();
   }
 }
@@ -58,7 +70,7 @@ function attachQuickReference1() {
     window.quickReference[details.details.uid] = {};
   }
   window.quickReference[details.details.uid][version.details.uid] = {
-    Main: QRBoilerplate,
+    Main: QRPointLineDistance,
     // QR2: QRBoilerplate2,
   };
 }
