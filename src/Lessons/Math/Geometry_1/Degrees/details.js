e93b7763ad8572c82598cd8aa1357ccf9dc73165
“airladon” <airladon@gmail.com>
2019-05-14 15:47:20 -0700

    initial degrees add

---
+++ src/Lessons/Math/Geometry_1/Degrees/details.js
@@ -0,0 +1,11 @@
+// @flow
+
+// eslint-disable-next-line no-var
+var details = {
+  title: 'Degrees',
+  dependencies: ['Angle'],
+};
+
+module.exports = {
+  details,
+};
---
+++ src/Lessons/Math/Geometry_1/Degrees/explanation/base/version.js
@@ -0,0 +1,13 @@
+// @flow
+
+// eslint-disable-next-line no-var
+var details = {
+  title: 'Base',
+  description: '',
+  fullLesson: true,
+  type: 'presentation',
+};
+
+module.exports = {
+  details,
+};
