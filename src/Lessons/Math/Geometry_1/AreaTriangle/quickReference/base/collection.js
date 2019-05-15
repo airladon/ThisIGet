9f6461879c1cbe8a36034cd6bd6e73c02e69ad4e
“airladon” <airladon@gmail.com>
2019-05-15 09:13:03 -0700

    add Area Triangle and split qr and snapshots

---
+++ src/Lessons/Math/Geometry_1/AreaTriangle/quickReference/base/collection.js
@@ -0,0 +1,47 @@
+// @flow
+import Fig from 'figureone';
+import CommonLessonDiagram from '../../../../../LessonsCommon/CommonLessonDiagram';
+import CommonDiagramCollection from '../../../../../LessonsCommon/DiagramCollection';
+
+const {
+  // DiagramElementPrimative,
+  DiagramObjectAngle,
+  DiagramObjectLine,
+  // DiagramObjectPolyLine,
+  DiagramElementCollection,
+  Transform,
+  // Point,
+  Equation,
+} = Fig;
+
+// const { round, rand } = Fig.tools.math;
+
+export default class CommonCollection extends CommonDiagramCollection {
+  _height1: {
+    _line: DiagramObjectLine;
+    _angle: DiagramObjectAngle;
+    _base: DiagramObjectLine;
+  } & DiagramElementCollection;
+
+  _height2: {
+    _line: DiagramObjectLine;
+    _angle: DiagramObjectAngle;
+    _base: DiagramObjectLine;
+    _baseExtension: DiagramObjectLine;
+  } & DiagramElementCollection;
+
+  _eqn: Equation;
+  base: number;
+  height: number;
+  area: number;
+
+  constructor(
+    diagram: CommonLessonDiagram,
+    layout: Object,
+    transform: Transform = new Transform('Common').rotate(0).translate(0, 0),
+  ) {
+    super(diagram, layout, transform);
+    this.setPosition(this.layout.position);
+    this.diagram.addElements(this, this.layout.addElements);
+  }
+}
