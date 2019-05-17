77753a9f089d9791706478aeeaf0dc0fe0bb9fd8
“airladon” <airladon@gmail.com>
2019-05-16 20:38:35 -0700

    updates except qr

--- src/Lessons/Math/Geometry_1/RightAngleTriangles/quickReference/base/lesson-dev.js
+++ src/Lessons/Math/Geometry_1/RightAngleTriangles/quickReference/base/lesson-dev.js
@@ -3,10 +3,11 @@
 import renderLesson from '../../../../../../js/views/lesson/lesson';
 import PresentationLesson from '../../../../../../js/Lesson/PresentationLesson';
 import Content from './content';
-import details from '../../details';
-import version from './version';
+// import details from '../../details';
+// import version from './version';
 import '../../../geometry.scss';
 
 const lesson = new PresentationLesson(new Content());
 
-renderLesson(lesson, details.details, version.details);
+renderLesson(lesson);
+
