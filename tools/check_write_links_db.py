b9f57ae34874e723cfb461a3296e0b3762c19516
“airladon” <airladon@gmail.com>
2019-05-20 09:01:08 -0700

    add set_app_env.py to set environment for app

--- tools/check_write_links_db.py
+++ tools/check_write_links_db.py
@@ -1,12 +1,23 @@
 import pathlib
 import re
 import json
+import os
 import sys
+from set_app_env import set_app_env
+
+app = sys.argv[-1]
+
+# if 'DATABASE_URL' in os.environ:
+#     print(os.environ['DATABASE_URL'])
+# else:
+#     print('DATABASE_URL not set')
+# print()
+set_app_env(app)
+# print()
+# print(os.environ['DATABASE_URL'])
+
 sys.path.insert(0, './app/')
 from app import app  # noqa
 from app.models import db, Versions, Lessons, Categories, Topics  # noqa
 
-
-# def check_write_links_db(show=True, write=False):
-def get_all_links():
-    
+print(db.engine.url)
