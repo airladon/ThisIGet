dcb1180e02290c358f7cf97b9eb37a1437fdf213
“airladon” <airladon@gmail.com>
2019-05-20 09:12:52 -0700

    loaded in json

--- tools/check_write_links_db.py
+++ tools/check_write_links_db.py
@@ -3,21 +3,27 @@ import re
 import json
 import os
 import sys
+import subprocess
 from set_app_env import set_app_env
 
 app = sys.argv[-1]
-
-# if 'DATABASE_URL' in os.environ:
-#     print(os.environ['DATABASE_URL'])
-# else:
-#     print('DATABASE_URL not set')
-# print()
 set_app_env(app)
-# print()
-# print(os.environ['DATABASE_URL'])
 
 sys.path.insert(0, './app/')
 from app import app  # noqa
 from app.models import db, Versions, Lessons, Categories, Topics  # noqa
 
-print(db.engine.url)
+print(f'Using database: ${db.engine.url}\n')
+print('Creating links index\n')
+
+all_links = subprocess.run(['node', './tools/getAllLinks.js'])
+
+if all_links.returncode != 0:
+    print(all_links.stderr)
+    raise Exception(f'Error creating links file')
+
+links = []
+with open('./tools/link_index.json') as f:
+    links = json.load(f)
+
+print(links)
