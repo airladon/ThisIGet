27406f64407476c48c481c0544672fd4f4c65fa6
“airladon” <airladon@gmail.com>
2019-05-23 19:28:43 -0700

    more cleanup

--- src/Lessons/Math/Geometry_1/RightAngleTriangles/explanation/tester/lesson.js
+++ src/Lessons/Math/Geometry_1/RightAngleTriangles/explanation/tester/lesson.js
@@ -1,11 +1,35 @@
 // // @flow
-
+// @flow
+// import Fig from 'figureone';
+import * as React from 'react';
+import SimpleLessonContent from '../../../../../../js/Lesson/SimpleLessonContent';
 import renderLesson from '../../../../../../js/views/lesson/lesson';
 import SimpleLesson from '../../../../../../js/Lesson/SimpleLesson';
-import Content from './content';
+import Markdown from '../../../../../../js/components/markdown';
+import imgLink from '../../tile.png';
+import imgLinkGrey from '../../tile-grey.png';
+import details from '../../details';
+import markdownContent from './content.md';
 import './style.scss';
-// import details from '../../details';
-// import version from './version';
+
+class Content extends SimpleLessonContent {
+  setTitle() {
+    this.title = details.details.title;
+    this.iconLink = imgLink;
+    this.iconLinkGrey = imgLinkGrey;
+  }
+
+  setContent() {
+    this.loadStaticQRs([
+      'Math/Geometry_1/Isosceles/base',
+      'Math/Geometry_1/RightAngleTriangles/static',
+    ]);
+
+    this.sections = [
+      <Markdown key={0} content={markdownContent}/>,
+    ];
+  }
+}
 
 const lesson = new SimpleLesson(new Content());
 renderLesson(lesson);
