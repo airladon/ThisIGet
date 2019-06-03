18c2b67f2d90de9fef164272a29d21dda2e15a3f
“airladon” <airladon@gmail.com>
2019-06-03 16:01:08 -0700

    fix congruent

--- src/Lessons/Math/Geometry_1/CongruentTriangles/explanation/base/tests/stage.btest.js
+++ src/Lessons/Math/Geometry_1/CongruentTriangles/explanation/base/tests/stage.btest.js
@@ -11,7 +11,7 @@ tester(
       34: { threshold: { next: 0.0001, prev: 0.005 } },
       35: { threshold: { next: 0.0001, prev: 0.005 } },
       36: { threshold: { next: 0.0001, prev: 0.0001 } },
-      37: { threshold: { next: 0.0001, prev: 0.0001 } },
+      37: { threshold: { next: 0.0002, prev: 0.0002 } },
     },
   },
   'goto',
