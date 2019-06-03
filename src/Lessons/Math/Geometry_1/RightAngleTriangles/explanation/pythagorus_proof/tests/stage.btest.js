7090ceb91dd59f2e641e61aba579f1311bb177f1
“airladon” <airladon@gmail.com>
2019-06-03 14:47:20 -0700

    update single page tests

--- src/Lessons/Math/Geometry_1/RightAngleTriangles/explanation/pythagorus_proof/tests/stage.btest.js
+++ src/Lessons/Math/Geometry_1/RightAngleTriangles/explanation/pythagorus_proof/tests/stage.btest.js
@@ -4,15 +4,15 @@ import tester from '../../../../../../testers/singlePageTester';
 
 tester(
   {
-    width: 300, height: 2500, scrollTo: 290,
+    width: 300, height: 2500, scrollTo: 280,
   },
   {
-    width: 300, height: 600, scrollTo: 290, includeQRs: true,
+    width: 300, height: 600, scrollTo: 280, includeQRs: true,
   },
-  {
-    width: 700, height: 3000, scrollTo: 200,
-  },
-  {
-    width: 700, height: 600, scrollTo: 200, includeQRs: true,
-  },
+  // {
+  //   width: 700, height: 3000, scrollTo: 200,
+  // },
+  // {
+  //   width: 700, height: 600, scrollTo: 200, includeQRs: true,
+  // },
 );
