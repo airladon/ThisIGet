882bf1be0bbacca165eb1b29bdebf42cc8caf274
“airladon” <airladon@gmail.com>
2019-06-03 20:58:53 -0700

    updates again

--- src/Lessons/Math/Geometry_1/Introduction/explanation/singlePage/tests/stage.btest.js
+++ src/Lessons/Math/Geometry_1/Introduction/explanation/singlePage/tests/stage.btest.js
@@ -3,16 +3,16 @@ import 'babel-polyfill';
 import tester from '../../../../../../testers/singlePageTester';
 
 tester(
-  // {
-  //   width: 300, height: 3150, scrollTo: 200,
-  // },
-  // {
-  //   width: 300, height: 600, scrollTo: 200, includeQRs: true,
-  // },
   {
     width: 700, height: 2500, scrollTo: 200,
   },
-  // {
-  //   width: 700, height: 600, scrollTo: 200, includeQRs: true,
-  // },
+  {
+    width: 300, height: 3150, scrollTo: 200,
+  },
+  {
+    width: 300, height: 600, scrollTo: 200, includeQRs: true,
+  },
+  {
+    width: 700, height: 550, scrollTo: 200, includeQRs: true,
+  },
 );
