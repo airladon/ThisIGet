be932254cfeca14bc16ebfe014f33ee1bd174404
“airladon” <airladon@gmail.com>
2019-06-04 19:01:03 -0700

    update snapshots

--- src/Lessons/Math/Geometry_1/RightAngleTriangles/explanation/pythagorus_proof/tests/stage.btest.js
+++ src/Lessons/Math/Geometry_1/RightAngleTriangles/explanation/pythagorus_proof/tests/stage.btest.js
@@ -3,7 +3,7 @@ import 'babel-polyfill';
 import tester from '../../../../../../testers/singlePageTester';
 
 tester(
-  { width: 300 },
+  { width: 300, threshold: 0.00001 },
   { width: 300, height: 450, includeQRs: true },
   { width: 700 },
   { width: 700, height: 450, includeQRs: true },
