ce09ef74f62d6f70edea90f79e17a58c58ddf1e7
“airladon” <airladon@gmail.com>
2019-06-14 20:23:24 -0700

    added hashes file

---
+++ tools/check_cdn_cache.py
@@ -0,0 +1,34 @@
+import os
+import hashlib
+import json
+import requests
+
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+
+file_list = {}
+for root, dirs, files in os.walk('./app/app/static'):
+    for file in files:
+        if os.path.isdir(file):
+            continue
+        full_file_path = os.path.join(root, file)
+        md5_str = md5(full_file_path)
+        file_list[full_file_path] = md5_str
+
+
+with open('./app/app/static/hashes.json', 'w') as outfile:
+    json.dump(file_list, outfile)
+
+
+r = requests.get('https://www.thisiget.com/static/hashes.json')
+
+if r.status_code != 200:
+    print('hashes do not exist at www.thisiget.com')
+    exit()
+
+# cdn_hashes = r.content
