8fb8eb9cb8bae9c3511b358c54e11bae3c197f4d
“airladon” <airladon@gmail.com>
2019-06-27 17:05:11 -0700

    add links

--- src/Lessons/LessonsCommon/lessonindex.js
+++ src/Lessons/LessonsCommon/lessonindex.js
@@ -593,6 +593,16 @@ export default function getLessonIndex() {
             fullLesson: true,
           },
         },
+        links: {
+          base: {
+            type: 'generic',
+            title: 'External',
+            description: 'External links to sources of information for Parallel Split of a Triangle',
+            htmlTitle: 'Links: Parallel split of a triangle',
+            htmlDescription: 'External links looking at triangle proportionality and parallel splits of a triangle.',
+            fullLesson: false,
+          },
+        },
         summary: {
           base: {
             type: 'presentation',
---
+++ src/Lessons/Math/Geometry_1/ParallelSplitOfTriangle/links/base/version.js
@@ -0,0 +1,44 @@
+// @flow
+
+// eslint-disable-next-line no-var
+var version = {
+  uid: 'base',
+  topic: 'links',
+  type: 'generic',
+  title: 'External',
+  description: 'External links to sources of information for Parallel Split of a Triangle',
+  htmlTitle: 'Links: Parallel split of a triangle',
+  htmlDescription: 'External links looking at triangle proportionality and parallel splits of a triangle.',
+  links: [
+    {
+      url: 'https://en.wikipedia.org/wiki/Intercept_theorem',
+      hash: '7e97eb0b63db85f2f93d98674bc6d560',
+      author: 'Intercept Theorem',
+      type: 'generic',
+      description: 'Proof for intercept theorem which is more general proportionality theorem',
+    },
+    {
+      url: 'https://sites.google.com/site/ncertstdx/similar-triangles-theorems',
+      hash: '11d70ead6e3138cd4b774ecfe9422867',
+      publisher: 'NCERT X Mathematics',
+      type: 'generic',
+      description: 'Proofs for why proportional triangles are similar, and why equiangular triangles are similar',
+    },
+    {
+      url: 'http://www.malinc.se/math/geometry/similartrianglesen.php',
+      hash: 'a3b9b446b25e753f5d806fe120435e85',
+      author: 'Malin Christersson',
+      type: 'generic',
+      description: 'Triangle Proportionality Theorem and Similar Triangles',
+    },
+    {
+      url: 'https://mathbitsnotebook.com/Geometry/Similarity/SMSplitter.html',
+      hash: '36ee83061172b3a471d90db4d5cfcfa0',
+      publisher: 'Math Bits Notebook',
+      type: 'generic',
+      description: 'Side Splitter Theorem and Converse',
+    },
+  ],
+};
+
+module.exports = version;
