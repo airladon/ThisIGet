ebc9be7c2d6b6f5886deaf9dade3b839faa7c4a9
“airladon” <airladon@gmail.com>
2019-07-12 11:45:48 -0700

    fix figureone

--- updateFigureOne.sh
+++ updateFigureOne.sh
@@ -3,6 +3,7 @@
 FIGURE_ONE_VERSION=`npm show figureone version`
 npm install figureone@$FIGURE_ONE_VERSION
 MD5=`md5 -q node_modules/figureone/figureone.min.js`
+rm app/app/static/figureone.min-*
 cp node_modules/figureone/figureone.min.js app/app/static/figureone.min-$MD5.js
 cat containers/Dockerfile_dev | sed "s/\(RUN npm install figureone@\).*/\1$FIGURE_ONE_VERSION/" > temp
 mv temp containers/Dockerfile_dev
