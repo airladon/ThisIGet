9cf70adf9251b0782b4935d3070f1330d57c8405
“airladon” <airladon@gmail.com>
2019-07-12 14:27:13 -0700

    update snapshots

--- src/Lessons/Math/Geometry_1/ExternalAngles/links/base/content.js
+++ src/Lessons/Math/Geometry_1/ExternalAngles/links/base/content.js
@@ -17,26 +17,23 @@ class Content extends SimpleLessonContent {
   setContent() {
     this.sections = [
       <div key="0" className="lesson__links_table__disclaimer">
-        <p>
-          {'These websites are not administered by This I Get. We don\'t control their availability, accuracy or policies.'}
+        <p style={{ marginBottom: '3em' }}>
+          {'A selection of external links that describe the same topic in different ways is below. If you visit a link, then you can rate the link for your own future reference, and for others that haven\'t yet visited it. If the link is broken or the content has changed and is no longer appropriate, then please let us know at '}
+            <a
+              href="mailto:feedback@thisiget.com?Subject=Feedback"
+              className="lesson__links_table__disclaimer_email"
+              >
+              feedback@thisiget.com
+            </a>
+            {'.'}
         </p>
         <p>
         </p>
       </div>,
       { links: version.links },
-      <div key="1" className="lesson__links_table__feedback">
-        <p>
-          {'If you visit a link, then you can rate the link for your own future reference, and for others that haven\'t yet visited it.'}
-        </p>
-        <p>
-        {'If the link is broken or the content has changed and is no longer appropriate, then please let us know at '}
-          <a
-            href="mailto:feedback@thisiget.com?Subject=Feedback"
-            className="lesson__links_table__disclaimer_email"
-            >
-            feedback@thisiget.com
-          </a>
-          {'.'}
+      <div key="1" className="lesson__links_table__disclaimer">
+        <p style={{ marginTop: '3em' }}>
+          {'Please note, these websites are not administered by This I Get. We don\'t control their availability, accuracy or policies.'}
         </p>
       </div>,
     ];
