39b20dc58ce373f4d03902bc2b79b0e012549a4b
“airladon” <airladon@gmail.com>
2019-07-15 11:17:02 -0700

    add boilerplate links

---
+++ src/Lessons/Math/Geometry_1/Degrees/links/base/version.js
@@ -0,0 +1,23 @@
+// @flow
+
+// eslint-disable-next-line no-var
+var version = {
+  uid: 'base',
+  topic: 'links',
+  type: 'generic',
+  title: 'External',
+  description: 'External links to sources of information for <TOPIC>',
+  htmlTitle: '<TOPIC> Links',
+  htmlDescription: 'Explanations, proofs, examples and questions about <TOPIC>',
+  links: [
+    {
+      url: '',
+      hash: 'd41d8cd98f00b204e9800998ecf8427e',
+      publisher: '',
+      type: 'generic',
+      description: '',
+    },
+  ],
+};
+
+module.exports = version;
