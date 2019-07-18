3a74c290ccc2414c689453f672d1ec9a57c1c9c0
“airladon” <airladon@gmail.com>
2019-07-18 12:31:21 -0700

    update angles at intersection examples with qrs

--- src/Lessons/Math/Geometry_1/AnglesAtIntersections/examples/base/content.js
+++ src/Lessons/Math/Geometry_1/AnglesAtIntersections/examples/base/content.js
@@ -14,7 +14,8 @@ class Content extends SimpleLessonContent {
     // this.iconLink = imgLink;
     // this.iconLinkGrey = imgLinkGrey;
     this.loadQRs([
-      // 'Math/Geometry_1/Triangles/base',
+      'Math/Geometry_1/AngleGroups/base',
+      // 'Math/Geometry_1/AnglesAtIntersections/base',
     ]);
   }
 
