507e3ac78e586036d8e589cc0ae09b07f9884cd0
“airladon” <airladon@gmail.com>
2019-07-18 12:52:24 -0700

    add links

---
+++ src/Lessons/Math/Geometry_1/Triangles/links/base/version.js
@@ -0,0 +1,44 @@
+// @flow
+
+// eslint-disable-next-line no-var
+var version = {
+  uid: 'base',
+  topic: 'links',
+  type: 'generic',
+  title: 'External',
+  description: 'External links to sources of information for Triangles>',
+  htmlTitle: 'Triangles Links',
+  htmlDescription: 'Explanations, proofs, examples and questions about Triangles',
+  links: [
+    {
+      url: 'https://www.splashmath.com/math-vocabulary/geometry/triangle',
+      hash: '',
+      publisher: 'Splash Math',
+      type: 'generic',
+      description: 'Introduction to Triangles',
+    },
+    {
+      url: 'https://www.mathplanet.com/education/pre-algebra/introducing-geometry/triangles',
+      hash: '',
+      publisher: 'Math Planet',
+      type: 'generic',
+      description: 'Introduction to Triangles',
+    },
+    {
+      url: 'https://en.wikipedia.org/wiki/Triangle',
+      hash: '',
+      publisher: 'Wikipedia',
+      type: 'generic',
+      description: 'Detailed description including definition, properties and history',
+    },
+    {
+      url: 'https://www.mathsisfun.com/triangle.html',
+      hash: '',
+      publisher: 'Math is Fun',
+      type: 'generic',
+      description: 'Definition, triangle types and interactive diagrams',
+    },
+  ],
+};
+
+module.exports = version;
