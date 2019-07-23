01636fdb639b82961e75441db3c64fd8688a38ea
“airladon” <airladon@gmail.com>
2019-07-22 17:16:57 -0700

    update tests

--- src/Lessons/Math/Geometry_1/ExternalAngles/discover/simple/tests/full.btest.js
+++ src/Lessons/Math/Geometry_1/ExternalAngles/discover/simple/tests/full.btest.js
@@ -6,5 +6,5 @@ tester(
   { width: 700 },
   { width: 700, height: 400, includeQRs: true },
   { width: 300 },
-  { width: 300, height: 400,  includeQRs: true },
+  { width: 300, height: 400, includeQRs: true },
 );
