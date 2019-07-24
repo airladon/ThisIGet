d3a132b10b77d5c68bfcb2c89cd18359fbd0d280
“airladon” <airladon@gmail.com>
2019-07-23 17:16:43 -0700

    links initial

---
+++ src/Lessons/Math/Geometry_1/Quadrangles/links/base/version.js
@@ -0,0 +1,23 @@
+// @flow
+
+// eslint-disable-next-line no-var
+var version = {
+  uid: 'base',
+  topic: 'links',
+  type: 'generic',
+  title: 'External',
+  description: 'External links to sources of information for quadrangles and quadrilaterals',
+  htmlTitle: 'Links for quadrangles and quadrilaterals',
+  htmlDescription: 'Links with explanations, examples and proofs quadrangles and quadrilaterals',
+  links: [
+    {
+      url: '',
+      hash: '',
+      publisher: '',
+      type: 'generic',
+      description: '',
+    },
+  ],
+};
+
+module.exports = version;
