a88223d7714c5ab2eecb661bfa7e2088c0be9464
“airladon” <airladon@gmail.com>
2019-07-23 21:17:42 -0700

    update thresholds

--- src/Lessons/Math/Geometry_1/Quadrangles/discover/simple/tests/full.btest.js
+++ src/Lessons/Math/Geometry_1/Quadrangles/discover/simple/tests/full.btest.js
@@ -3,8 +3,12 @@ import 'babel-polyfill';
 import tester from '../../../../../../testers/singlePageTester';
 
 tester(
-  { width: 700 },
-  { width: 700, height: 400, includeQRs: true },
-  { width: 300 },
-  { width: 300, height: 400, includeQRs: true },
+  { width: 700, threshold: 0.0001 },
+  {
+    width: 700, height: 400, includeQRs: true, threshold: 0.0001,
+  },
+  { width: 300, threshold: 0.0001 },
+  {
+    width: 300, height: 400, includeQRs: true, threshold: 0.0001,
+  },
 );
