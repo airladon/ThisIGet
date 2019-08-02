867a663707785b776222b89ba2a3839506cf83d8
“airladon” <airladon@gmail.com>
2019-08-02 06:28:01 -0700

    update boilerplates

---
+++ src/Lessons/boilerplate/examples/base/content.js
@@ -0,0 +1,32 @@
+// @flow
+// import Fig from 'figureone';
+import * as React from 'react';
+import SimpleLessonContent from '../../../../../../js/Lesson/SimpleLessonContent';
+// import imgLink from '../../tile.png';
+// import imgLinkGrey from '../../tile-grey.png';
+import details from '../../details';
+// $FlowFixMe
+import content from './content.md';
+
+class Content extends SimpleLessonContent {
+  setTitle() {
+    this.title = details.title;
+    //  this.iconLink = imgLink;
+    //  this.iconLinkGrey = imgLinkGrey;
+    this.loadQRs([
+      // 'Math/Geometry_1/Triangles/base',
+      // 'Math/Geometry_1/AnglesAtIntersections/base/',
+      // 'Math/Geometry_1/AngleGroups/base/',
+    ]);
+  }
+
+  setContent() {
+    this.sections = [
+      <div key={0}>
+      <div className="markdown" dangerouslySetInnerHTML={ { __html: content } }/>
+      </div>,
+    ];
+  }
+}
+
+export default Content;
---
+++ src/Lessons/boilerplate/examples/base/content.md
@@ -0,0 +1,13 @@
+## Title
+
+
+### Example 1:
+
+#### What is the size of angle $$\definecolor{r}{RGB}{238,34,12}\bbox[10px,border:1px solid gray]{a}$$ in degrees?
+
+<!-- ![](ex1.png)
+
+<hintDown>[Answer]
+[[triangle]]((qr,'Math/Geometry_1/Triangles/base/AngleSum',#00756F)) 
+</hintDown>
+ -->
