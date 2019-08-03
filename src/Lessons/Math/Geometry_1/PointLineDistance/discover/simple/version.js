0332491b4081c030be3f8536895378c7cf5759d7
“airladon” <airladon@gmail.com>
2019-08-02 19:21:08 -0700

    updates

---
+++ src/Lessons/Math/Geometry_1/PointLineDistance/discover/simple/version.js
@@ -0,0 +1,15 @@
+// @flow
+
+// eslint-disable-next-line no-var
+var version = {
+  uid: 'simple',
+  topic: 'discover',
+  title: 'Base',
+  description: '',
+  htmlTitle: 'Self discover the angle of the distance between a point and line',
+  htmlDescription: 'Figure out why ...',
+  fullLesson: true,
+  type: 'singlePage',
+};
+
+module.exports = version;
