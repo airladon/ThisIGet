2fe9006079925f43f78ed12d1e29574c24bc1d2f
airladon <airladon@gmail.com>
2019-09-14 08:05:40 -0700

    update prepopulate script

--- tools/prepopulate.py
+++ tools/prepopulate.py
@@ -4,12 +4,12 @@ sys.path.insert(0, './app/')
 from app import app  # noqa
 from app.models import db, Users, VersionRatings # noqa
 from app.tools import hash_str_with_pepper  # noqa
+from app.routes import update_version_rating_cache  # noqa
 
 
 def addUser(username, email, password):
     user = Users.query.filter_by(
         username_hash=hash_str_with_pepper(username.lower())).first()
-    # user = Users.query.filter_by(username=username).first()
     if user is None:
         print(f'Adding {username}')
         user = Users()
@@ -33,23 +33,21 @@ def addRating(version_uid, rating_value, username):
     db.session.add(rating)
 
 
-# addUser('airladon', 'airladon@gmail.com', 'asdfasdf')
 for i in range(10):
     username = f'test_user_{i:03}'
     email = f'{username}@thisiget.com'
     password = '12345678'
     addUser(username, email, password)
 
-addRating('/Math/Geometry_1/Equilateral/explanation/base', 1, 'test_user_001')
-addRating('/Math/Geometry_1/Equilateral/explanation/base', 4, 'test_user_002')
+addRating('Math/Geometry_1/Equilateral/explanation/base', 1, 'test_user_001')
+addRating('Math/Geometry_1/Equilateral/explanation/base', 2, 'test_user_002')
+addRating('Math/Geometry_1/Equilateral/explanation/base', 3, 'test_user_003')
+addRating('Math/Geometry_1/Equilateral/explanation/base', 4, 'test_user_004')
+addRating('Math/Geometry_1/Equilateral/explanation/base', 5, 'test_user_005')
+addRating('Math/Geometry_1/Equilateral/explanation/base', 1, 'test_user_006')
+addRating('Math/Geometry_1/Equilateral/explanation/base', 2, 'test_user_007')
+addRating('Math/Geometry_1/Equilateral/explanation/base', 3, 'test_user_008')
+addRating('Math/Geometry_1/Equilateral/explanation/base', 4, 'test_user_009')
+addRating('Math/Geometry_1/Equilateral/explanation/base', 5, 'test_user_009')
+update_version_rating_cache('Math/Geometry_1/Equilateral/explanation/base')
 db.session.commit()
-# addRating('Equilateral', 'explanation', 'base', 1, 'test_user_000')
-# addRating('Equilateral', 'explanation', 'base', 2, 'test_user_001')
-# addRating('Equilateral', 'explanation', 'base', 3, 'test_user_002')
-# addRating('Equilateral', 'explanation', 'base', 4, 'test_user_003')
-# addRating('Equilateral', 'explanation', 'base', 5, 'test_user_004')
-# addRating('Equilateral', 'explanation', 'base', 1, 'test_user_005')
-# addRating('Equilateral', 'explanation', 'base', 1, 'test_user_006')
-# addRating('Equilateral', 'explanation', 'base', 3, 'test_user_007')
-# addRating('Equilateral', 'explanation', 'base', 4, 'test_user_008')
-# addRating('Equilateral', 'explanation', 'base', 5, 'test_user_009')
