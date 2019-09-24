1f8f86c0c1739a4c4305ef05fc89d2b3a3c2d80b
airladon <airladon@gmail.com>
2019-09-23 17:08:43 -0700

    updates

--- src/js/components/navbar.js
+++ src/js/components/navbar.js
@@ -164,7 +164,7 @@ export default class Navbar extends React.Component
       return <a className="navbar-icon-container"
          href="/">
         <img className="navbar-icon"
-             src="/static/assets/logo20.svg"
+             src="/static/assets/logo20v3.svg"
              alt="navbar home icon"/>
       </a>;
     }
