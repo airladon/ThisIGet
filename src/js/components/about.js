eb175c4f12f80a6b3c3f82820064ccc1e56a3442
airladon <airladon@gmail.com>
2019-09-24 07:04:05 -0700

    updates

--- src/js/components/about.js
+++ src/js/components/about.js
@@ -15,59 +15,44 @@ export default class About extends React.Component
       </div>*/}
       <div className="about__tiles">
         <div className="about__tiles_text">
-          There are different approaches to learning
+          <div className="about__tiles_text_table">
+            <div className="about__tiles_text_container">
+              <div className="about__tiles_text_title">
+                There are different approaches to learning
+              </div>
+              <div className="about__tiles_text_text">
+                Depending on the learner or application, different appraoches might be more efficient
+              </div>
+            </div>
+          </div>
         </div>
         <div className="about__tiles_tiles">
-          <table className="about__tiles_table">
-            <tbody>
-            <tr className="about__tiles_tile_row">
-              <td className="about__tiles_tile_col">
-                <div className="about__tiles_tile_cell" id="about__tile_explanation">Explanation</div>
-              </td>
-              <td className="about__tiles_tile_col">
-                <div className="about__tiles_tile_cell" id="about__tile_discovery">Discovery</div>
-              </td>
-            </tr>
-            <tr className="about__tiles_tile_row">
-              <td className="about__tiles_tile_col">
-                <div className="about__tiles_tile_cell" id="about__tile_practice">Practice</div>
-              </td>
-              <td className="about__tiles_tile_col">
-                <div className="about__tiles_tile_cell" id="about__tile_examples">Examples</div>
-              </td>
-            </tr>
-            </tbody>
-          </table>
+          <div className="about__tiles_group">
+            <div className="about__tiles_tile_col">
+              <div className="about__tiles_tile_cell" id="about__tile_explanation">
+                Explanation
+              </div>
+            </div>
+            <div className="about__tiles_tile_col">
+              <div className="about__tiles_tile_cell" id="about__tile_discovery">
+                Discovery
+              </div>
+            </div>
+          </div>
+          <div className="about__tiles_group">
+            <div className="about__tiles_tile_col">
+              <div className="about__tiles_tile_cell" id="about__tile_practice">
+                Practice
+              </div>
+            </div>
+            <div className="about__tiles_tile_col">
+              <div className="about__tiles_tile_cell" id="about__tile_examples">
+                Examples
+              </div>
+            </div>
           </div>
         </div>
-      <div className="about__tiles">
-        
-        <div className="about__tiles_tiles">
-          <table className="about__tiles_table">
-            <tbody>
-            <tr className="about__tiles_tile_row">
-              <td className="about__tiles_tile_col">
-                <div className="about__tiles_tile_cell" id="about__tile_precise">Precise</div>
-              </td>
-              <td className="about__tiles_tile_col">
-                <div className="about__tiles_tile_cell" id="about__tile_interactive">Interactive</div>
-              </td>
-            </tr>
-            <tr className="about__tiles_tile_row">
-              <td className="about__tiles_tile_col">
-                <div className="about__tiles_tile_cell" id="about__tile_intuitive">Intuitive</div>
-              </td>
-              <td className="about__tiles_tile_col">
-                <div className="about__tiles_tile_cell" id="about__tile_detailed">Detailed</div>
-              </td>
-            </tr>
-            </tbody>
-          </table>
-          </div>
-          <div className="about__tiles_text">
-            There are many different ways to describe something
-          </div>
-        </div>
+      </div>
     </div>;
   }
 }
