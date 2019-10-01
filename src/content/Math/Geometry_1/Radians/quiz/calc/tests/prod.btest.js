1e6cffeb0a9acf5cdeffe43d981638b768af6fe6
airladon <airladon@gmail.com>
2019-10-01 16:37:11 -0700

    remove calc quiz test as it keeps making different sized images

--- src/content/Math/Geometry_1/Radians/quiz/calc/tests/prod.btest.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import 'babel-polyfill';
-import tester from '../../../../../../testers/singlePageTester';
-
-tester(
-  { width: 700, threshold: 1500 },
-);
