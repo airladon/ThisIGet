a14af4289765dcadcdcf3b7e2b10748ff05290cb
airladon <airladon@gmail.com>
2019-10-03 06:25:38 -0700

    fix lint

--- app/app/forms.py
+++ app/app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms import BooleanField
+# # Uncomment for Privacy
+# from wtforms import BooleanField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 # from wtforms.validators import Length
 from app.models import Users
