71eb4224ce32d1a14de5eba7a2be5e87c66e151e
airladon <airladon@gmail.com>
2019-10-23 15:49:34 -0700

    trigger

--- start_env.sh
+++ start_env.sh
@@ -136,10 +136,12 @@ then
   then
     DOCKER_GROUP_ID=`grep -e '^docker:' /etc/group | sed 's/[^:]*:[^:]*:\([0-9]*\).*/\1/'`
   fi
+
   if [ -z "$DOCKER_GROUP_ID" ];
   then
     DOCKER_GROUP_ID=`ls -n /var/run/docker.sock | sed "s/[^ ]* *[^ ]* *\([^ ]*\).*/\1/"`
   fi
+  
   HOST_USER_GROUP_ID=`id -g`
   HOST_USER_ID=`id -u`
 
