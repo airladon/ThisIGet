cc2641c929dd06121c64839fd195caf126cfc633
airladon <airladon@gmail.com>
2019-11-04 12:55:47 -0800

    update links

--- src/content/Math/Geometry_1/SimilarTriangles/links/base/version.js
+++ src/content/Math/Geometry_1/SimilarTriangles/links/base/version.js
@@ -17,7 +17,27 @@ var version = {
       type: 'generic',
       description: 'Proofs for why similar triangles have equal corresponding angles, and why triangles with equal corresponding angles are similar',
     },
+    {
+      url: 'https://en.wikipedia.org/wiki/Similarity_(geometry)',
+      publisher: 'Wikipedia',
+      type: 'generic',
+      description: 'Similarity in geometry',
+    },
+    {
+      url: 'https://www.mathopenref.com/similartriangles.html',
+      publisher: 'Math Open Reference',
+      type: 'generic',
+      description: 'Introduction to similar triangles and their tests, including interactive applet',
+    },
+    {
+      url: 'https://mathbitsnotebook.com/Geometry/Similarity/SMNumber.html',
+      publisher: 'Math Bits Notebook',
+      type: 'generic',
+      description: 'Example problems using similar triangles',
+    },
   ],
 };
 
 module.exports = version;
+
+
