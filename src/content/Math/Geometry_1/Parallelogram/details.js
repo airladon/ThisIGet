cfefcafae6f12899e59d08b70b446e4289e4be05
airladon <airladon@gmail.com>
2019-11-06 14:37:55 -0800

    add topic

---
+++ src/content/Math/Geometry_1/Parallelogram/details.js
@@ -0,0 +1,12 @@
+// @flow
+
+// eslint-disable-next-line no-var
+var topicDetails = {
+  title: 'Parallelogram',
+  dependencies: [
+    'AreaTriangle',
+  ],
+  enabled: true,
+};
+
+module.exports = topicDetails;
