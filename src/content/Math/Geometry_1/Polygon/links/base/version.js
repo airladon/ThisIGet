8915b3cf5809907a74f11372644836567a5ad608
airladon <airladon@gmail.com>
2019-11-18 20:37:26 -0800

    add links

--- src/content/Math/Geometry_1/Polygon/links/base/version.js
+++ src/content/Math/Geometry_1/Polygon/links/base/version.js
@@ -24,6 +24,27 @@ var version = {
       type: 'generic',
       description: 'General information on regular polygons',
     },
+    {
+      url: 'https://www.qc.edu.hk/math/Junior%20Secondary/interior%20angle.htm',
+      hash: '',
+      publisher: 'Queens College HK',
+      type: 'generic',
+      description: 'Proofs for internal angle sums',
+    },
+    {
+      url: 'https://mathbitsnotebook.com/Geometry/Polygons/POpolygonAnglePractice.html',
+      hash: '',
+      publisher: 'MathBitsNotebook',
+      type: 'generic',
+      description: 'Example problems with polygons',
+    },
+    {
+      url: 'https://www.mathsisfun.com/geometry/polygons.html',
+      hash: '',
+      publisher: 'MathBitsNotebook',
+      type: 'generic',
+      description: 'Polygon introduction',
+    },
   ],
 };
 
