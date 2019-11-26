e86cf06fb2b364525e3f14322fe52b951cfce456
airladon <airladon@gmail.com>
2019-11-25 17:48:04 -0800

    update qr

--- src/content/Math/Trigonometry_1/306090Triangle/quickReference/base/external.md
+++ src/content/Math/Trigonometry_1/306090Triangle/quickReference/base/external.md
@@ -1,3 +1,3 @@
 ![](fig.png)
 
-A triangle with angles 30º, 60º and 90º has opposite sides with ratios of 1, √3 and 2 respectively. This is commonly called a 30-60-90 triangle.
+A triangle with angles 30º, 60º and 90º has opposite sides with proportions of 1, √3 and 2 respectively. This is commonly called a 30-60-90 triangle.
--- src/content/Math/Trigonometry_1/306090Triangle/quickReference/base/quickReference.js
+++ src/content/Math/Trigonometry_1/306090Triangle/quickReference/base/quickReference.js
@@ -37,7 +37,7 @@ export default class QRMainPres extends PopupBoxCollection {
       '90º': highlight(colors.angles),
     };
     this.setTitle('30-60-90 Triangle');
-    this.setDescription('A triangle with angles |30º|, |60º| and |90º| has |opposite sides with ratios| of |1|, |√3| and |2| respectively. This is commonly called a |30-60-90 triangle|.', modifiers);
+    this.setDescription('A triangle with angles |30º|, |60º| and |90º| has |opposite sides with proportions| of |1|, |√3| and |2| respectively. This is commonly called a |30-60-90 triangle|.', modifiers);
     this.setLink(`${details.path}/${details.uid}/explanation/base?page=1`);
   }
 
