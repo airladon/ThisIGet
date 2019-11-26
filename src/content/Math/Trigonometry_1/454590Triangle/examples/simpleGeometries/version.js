e6f623ce7bbf79656480356be6eb4c0805e4f78c
airladon <airladon@gmail.com>
2019-11-25 20:41:27 -0800

    add simple geometries explanations

---
+++ src/content/Math/Trigonometry_1/454590Triangle/examples/simpleGeometries/version.js
@@ -0,0 +1,15 @@
+// @flow
+
+// eslint-disable-next-line no-var
+var version = {
+  uid: 'simpleGeometries',
+  topic: 'examples',
+  title: 'Simple Geometries',
+  description: '',
+  htmlTitle: '45-45-90 triangle basic usage',
+  htmlDescription: 'Simple geometries where the 45-45-90 triangle can be used for a solution',
+  fullTopic: true,
+  type: 'singlePage',
+};
+
+module.exports = version;
