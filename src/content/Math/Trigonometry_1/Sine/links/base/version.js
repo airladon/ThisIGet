e19913c596dd8822a385b7e176c4f8ee05aa98bc
airladon <airladon@gmail.com>
2020-02-13 17:51:34 -0800

    add links

--- src/content/Math/Trigonometry_1/Sine/links/base/version.js
+++ src/content/Math/Trigonometry_1/Sine/links/base/version.js
@@ -6,17 +6,39 @@ var version = {
   topic: 'links',
   type: 'generic',
   title: 'External',
-  description: 'External links to sources of information for <TOPIC>',
-  htmlTitle: '<TOPIC> Links',
-  htmlDescription: 'Explanations, proofs, examples and questions about <TOPIC>',
+  description: 'External links to sources of information for the sine function>',
+  htmlTitle: 'Sine function introduction Links',
+  htmlDescription: 'Explanations, proofs, examples and questions about the sine function>',
   links: [
     {
-      url: '',
+      url: 'https://www.mathopenref.com/sine.html',
       hash: 'd41d8cd98f00b204e9800998ecf8427e',
-      publisher: '',
+      publisher: 'Math Open Reference',
       type: 'generic',
-      description: '',
+      description: 'Sine definition and examples',
     },
+    {
+      url: 'https://en.wikipedia.org/wiki/Sine',
+      hash: 'd41d8cd98f00b204e9800998ecf8427e',
+      publisher: 'Wikipedia',
+      type: 'generic',
+      description: 'Sine definition and extensions',
+    },
+    {
+      url: 'https://www.mathsisfun.com/sine-cosine-tangent.html',
+      hash: 'd41d8cd98f00b204e9800998ecf8427e',
+      publisher: 'Math is Fun',
+      type: 'generic',
+      description: 'Introduction to Sine, Cosine and Tangent',
+    },
+    {
+      url: 'https://www.researchgate.net/publication/304168681_ELEMENTARY_DERIVATION_OF_SINE_AND_COSINE_SERIES',
+      hash: 'd41d8cd98f00b204e9800998ecf8427e',
+      author: 'David Bhatt',
+      type: 'generic',
+      description: 'Derivation of the sine function',
+    },
+
   ],
 };
 
