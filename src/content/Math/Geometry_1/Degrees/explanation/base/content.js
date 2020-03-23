2ad392cee5769cd5919b527a0a7cef5ae2da15e5
airladon <airladon@gmail.com>
2020-03-23 15:42:49 -0700

    update Angle

--- src/content/Math/Geometry_1/Degrees/explanation/base/content.js
+++ src/content/Math/Geometry_1/Degrees/explanation/base/content.js
@@ -133,7 +133,7 @@ class Content extends PresentationFormatContent {
       setContent: [
         'For example, it could be |_12| equal portions like a clock.',
       ],
-      modifiers: { _12: click(diag.pulseMarks, [diag, 12], colors.marks) },
+      modifiers: { _12: click(diag.pulseMarks, [diag, '12Long'], colors.marks) },
       show: [
         circle._line1, circle._line2, circle._angle, circle._marks12Long,
       ],
