fdbc2e12432132a5ed3a13cbc1651163ba7163fc
airladon <airladon@gmail.com>
2020-04-06 09:56:21 -0700

    cleanup local db

--- tools/get_users.py
+++ tools/get_users.py
@@ -5,8 +5,12 @@ from app import app  # noqa
 from app.models import db, Users # noqa
 
 # User is the name of table that has a column name
-users = Users.query.all()
+users = Users.query.filter(Users.username_hash!='deleted account').all()
+deleted_accounts = Users.query.filter(Users.username_hash=='deleted account').all()
+
+for user in users:
+    if user.username_hash != 'deleted account':
+      print('{:<10}{:<30}{:<40}{:<40}{:<5}'.format(user.id, user.get_username(), user.get_email(), str(user.signed_up_on), str(user.confirmed)))
 
 print(f'Number of users: {len(users)}')
-for user in users:
-    print (user.username[0:10], user.signed_up_on, user.confirmed)
+print(f'Number of deleted accounts: {len(deleted_accounts)}')
