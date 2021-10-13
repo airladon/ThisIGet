d8a5329efb36def88b0accd1f9a6d30badff325d
airladon <airladon@gmail.com>
2021-10-12 19:00:51 -0700

    test: update browser jest ignore for boilerplate

--- containers/playwright/jest.config.js
+++ containers/playwright/jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     '/src/.*\\.btest\\.js',
     '.*\\.btest\\.js',
   ],
-  testPathIgnorePatterns: ['/node_modules/', '\\.snap$'],
+  testPathIgnorePatterns: ['/node_modules/', '/src/content/boilerplate/', '\\.snap$'],
   testEnvironmentOptions: {
     'jest-playwright': {
       browsers: ['chromium'],
